72fdf56ba57a309a564c4e4b0a92c555b96cee35
sravani99 <sravaniyadala99@gmail.com>
2018-09-23 12:58:45 +0530

    basic practice

---
+++ angularjs/binding/src/app/test/test.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector:'app-test',
+    template: ''         ,
+      
+    styles:[]
+
+})
+export class TestComponent {
+  title ='binding'  
+}
